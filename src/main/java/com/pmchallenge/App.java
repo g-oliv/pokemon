abb2dcf6a4b5d9a67884505b0dea6f21b69ebe0b
Gonçalo Oliveira <goncalo.m.g.oliveira@gmail.com>
2022-09-12 13:18:20 +0100

    Initial commit

---
+++ src/main/java/com/pmchallenge/App.java
@@ -0,0 +1,12 @@
+package com.pmchallenge;
+
+/**
+ * App runner
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
